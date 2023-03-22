c28688fd6e7d163254dea6e8685f98862c989556
Richard <rmccormick314@gmail.com>
2023-03-22 16:16:57 -0700

    fixed dockerfile, added parser stuff

---
+++ bin/ISIS3_Enums.py
@@ -0,0 +1,8 @@
+# Python 3 Enums for ISIS3 Modules
+
+# Import dependencies ==========================================================
+from enum import Enum
+
+# Declare variables ============================================================
+
+# Declare functions ============================================================
---
+++ bin/json_parser.py
@@ -0,0 +1,28 @@
+# Python JSON Parser
+
+# Import dependencies ==========================================================
+import json
+
+# Declare variables ============================================================
+
+# Declare functions ============================================================
+"""
+Function    : read_file
+Description : Opens and reads a given file using the Python JSON parser/handler.
+Inputs      : filename (string)
+              - String representation of file name to be opened, including ext
+Outputs     : json_data (returned)
+              - Returns JSON object containing file data.
+"""
+def open_file(filename):
+    return json_data
+
+
+"""
+Function    : parse_file
+Description : Opens and reads a given file using the Python JSON parser/handler.
+Inputs      : filename (string)
+              - String representation of file name to be opened, including ext
+Outputs     : json_data (returned)
+              - Returns JSON object containing file data.
+"""
