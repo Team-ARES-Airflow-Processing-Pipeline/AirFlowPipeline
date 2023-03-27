3ebcd864b2d7afc3eed92965f20fe58902ee6834
Richard <rmccormick314@gmail.com>
2023-03-27 16:10:13 -0700

    finished basic parser

--- bin/ISIS3_Enums.py
+++ bin/ISIS3_Enums.py
@@ -4,5 +4,20 @@
 from enum import Enum
 
 # Declare variables ============================================================
+isis = ''
+
 
 # Declare functions ============================================================
+class ISIS_DAGS(Enum):
+    def __str__(self):
+        return str(self.value)
+    isis_mroctx2isis = 1
+    isis_catlab = 2
+    isis_spiceinit = 3
+    isis_footprintinit = 4
+    isis_caminfo = 5
+    isis_ctxcal = 6
+    isis_ctxevenodd = 7
+    cube_rename = 8
+    isis_cam2map = 9
+    gdal_translate = 10
--- bin/json_parser.py
+++ bin/json_parser.py
@@ -2,8 +2,10 @@
 
 # Import dependencies ==========================================================
 import json
+from ISIS3_Enums import ISIS_DAGS
 
 # Declare variables ============================================================
+JSON_DATA = ''
 
 # Declare functions ============================================================
 """
@@ -14,15 +16,44 @@ Inputs      : filename (string)
 Outputs     : json_data (returned)
               - Returns JSON object containing file data.
 """
-def open_file(filename):
-    return json_data
+def read_file(filename):
+    file = open(filename)
+
+    data = json.load(file)
+
+    file.close()
+
+    JSON_DATA = data
+    return data
 
 
 """
-Function    : parse_file
-Description : Opens and reads a given file using the Python JSON parser/handler.
-Inputs      : filename (string)
-              - String representation of file name to be opened, including ext
-Outputs     : json_data (returned)
-              - Returns JSON object containing file data.
+Function    : parse_data
+Description : Parses read JSON data into lists.
+Inputs      : data ( dict )
+              - Dictionary representation of a JSON file
+Outputs     : parsed_data (list)
+              - Parsed list of data / recipes.
 """
+def parse_data(data):
+    parsed_data = []
+
+    for key in data:
+        if( isinstance(data[key], dict) ):
+            recipeName = key
+            recipe = []
+            recipe.append(recipeName)
+
+            for keytwo in data[ recipeName ]:
+                if( isinstance(data[recipeName][keytwo], dict) ):
+                    for keythree in data[recipeName][keytwo]:
+                        recipe.append(getattr(ISIS_DAGS, keythree.replace('.', '_') ).value)
+                        for keyfour in data[recipeName][keytwo][keythree]:
+                            print(keyfour)
+
+            parsed_data.append( recipe )
+
+    return parsed_data
+
+data = read_file('example_recipes.json')
+print( parse_data(data) )
